14f38dbb59a9e27a3777a9967e05a37f2cf66360
Steve bob <mesivte1@gmail.com>
2022-08-18 13:43:28 -0400

    branch addtion

--- c-vibs.c
+++ c-vibs.c
@@ -5,4 +5,6 @@ int main()
 {
     char string[] = "hello to the funny world.";
     printf("Printing out a string %s\n", string);
+    char *pstring = "pointer to a string is stored in read only memory";
+    printf("%s\n", pstring);
 }
